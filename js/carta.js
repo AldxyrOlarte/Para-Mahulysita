303e8f9e880439835276eff6291daa34c925e076
Thiago Olarte <cwtgolds@gmail.com>
2026-06-20 11:22:55 -0500

    Update carta.js

--- js/carta.js
+++ js/carta.js
@@ -1,4 +1,4 @@
-const texto = `Hola mi amor.
+const texto = `Hola mi amor ❤️
 
 Quería escribirte esta pequeña carta para recordarte lo especial que eres para mí.
 
@@ -6,22 +6,23 @@ Gracias por cada sonrisa, cada conversación y cada momento que hemos compartido
 
 Eres una persona increíble y espero seguir creando muchos recuerdos hermosos contigo.
 
-Te quiero muchísimo. ❤️`;
+Te quiero muchísimo. 💖`;
 
 let i = 0;
 
-function escribirTexto(){
-    if(i < texto.length){
+function escribirTexto() {
+    if (i < texto.length) {
         document.getElementById("mensaje").innerHTML += texto.charAt(i);
         i++;
         setTimeout(escribirTexto, 45);
     }
 }
 
-function abrirCarta(){
+function abrirCarta() {
+
     document.getElementById("sobre").classList.toggle("abierto");
 
-    if(i === 0){
+    if (i === 0) {
         setTimeout(escribirTexto, 1000);
     }
 }
